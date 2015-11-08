39df6fca6f77cd1b083afc1bef2277d46c335b18
Joseph D Rivera MM <josephdriveramm@massmutuals-macbook-pro.local>
2015-11-08 10:14:22 -0500

    Initial Commit

---
+++ Joseph Warmup 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Joseph Warmup 01
+//
+//  Created by Joseph D Rivera on 11/8/15.
+//  Copyright © 2015 CoolDevApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
